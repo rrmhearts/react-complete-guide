b04a01bdb898762cf138152976085a953aa04a0f
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-06 14:25:52 -0400

    persons component before implmenting state.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './components/Person'
 
 class App extends Component {
+  /* only class Components, state reserved word*/
+  state = {
+    persons: [
+      { name: 'Ryan', age: 28 },
+      { name: 'Zoe', age: 29 },
+      { name: 'Alice', age: 26 }
+    ]
+
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <button>Switch Name</button>
+        <Person name="Ryan" age={Math.floor(Math.random() *30)}/>
+        <Person name="Alice" age={Math.floor(Math.random() *30)}/>
+        <Person name="Zoe" age={Math.floor(Math.random() *30)}>My hobby is sports!</Person>
       </div>
     );
   }
