7ec8cae12b4ae10b3228bf89b363f62552358cf6
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-07 15:32:48 -0400

    name chaned handler and CSS.

--- src/App.js
+++ src/App.js
@@ -27,12 +27,34 @@ class App extends Component {
     });
   };
 
+  nameChangedHandler = (event) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    });
+  };
+
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Ryan')}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Ryan')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -41,6 +63,7 @@ class App extends Component {
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
           click={this.switchNameHandler.bind(this, 'Max')} // preferred syntax
+          changed={this.nameChangedHandler}
         >
           My Hobbies: Racing
         </Person>
