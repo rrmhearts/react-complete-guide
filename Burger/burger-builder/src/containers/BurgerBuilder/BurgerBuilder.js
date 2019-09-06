5b1475cb3c6d4e2afc552acb0d2239377913e18a
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-09-06 15:08:05 -0400

    modal and order summary on order.

--- Burger/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.6,
@@ -18,7 +19,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState(ingredients) {
@@ -67,7 +69,12 @@ class BurgerBuilder extends Component {
         });
         this.updatePurchaseState(updatedIng);
     }
-    
+
+    /* Need an arrow function so that this gets bound to the right object.*/
+    purchaseHandler = () => {
+        this.setState({purchasing: true})
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -77,11 +84,15 @@ class BurgerBuilder extends Component {
         }
         return (
             <React.Fragment>
+                <Modal show={this.state.purchasing}>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls ingAdded={this.addIngredientHandler} 
                                ingRemoved={this.removeIngredientHandler}
                                disabled={disabledInfo}
                                purchasable={this.state.purchasable}
+                               ordered={this.purchaseHandler}
                                price={this.state.totalPrice} />
             </React.Fragment>
         );
