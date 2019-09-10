acbbce8bb453eb74fd6a5b41fa77d8ace6e777a3
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-09-10 15:10:18 -0400

    Reorganize for Routing. React-Router addition.

--- HTTP/src/containers/Blog/Blog.js
+++ HTTP/src/containers/Blog/Blog.js
@@ -1,69 +1,38 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostID: 0,
-        error: false
-    }
 
-    componentDidMount() {
-        axios.get('/posts') // async return promise
-            .then(response => {
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Ryan M'
-                    }
-                });
-                this.setState({
-                    posts: updatedPosts
-                });
-                //sconsole.log(response);
-            })
-            .catch(error => {
-                //console.log(error);
-                this.setState({
-                    error: true
-                })
-            });
-    }
 
-    postSelected = (id) => {
-        this.setState({selectedPostID: id})
-    }
+
 
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong.</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post key={post.id} 
-                            title={post.title} 
-                            author={post.author}
-                            clicked={() => this.postSelected(post.id)}/>
-            });
-        }
+
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostID}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+
+                        </ul>
+                    </nav>
+                </header>
+                <Posts />
             </div>
         );
     }
 }
 
 export default Blog;
+/*
+                <section>
+                    <FullPost id={this.state.selectedPostID}/>
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+                */
---
+++ HTTP/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+
+import Post from '../../../components/Post/Post';
+
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('/posts') // async return promise
+            .then(response => {
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Ryan M'
+                    }
+                });
+                this.setState({
+                    posts: updatedPosts
+                });
+                //sconsole.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    postSelected = (id) => {
+        this.setState({selectedPostID: id})
+    }
+
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong.</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post key={post.id} 
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() => this.postSelected(post.id)}/>
+            });
+        }
+        return (
+            <section className="Posts">
+               {posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;
