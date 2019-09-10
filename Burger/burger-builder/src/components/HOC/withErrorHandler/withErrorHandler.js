252df3aef0b5245320f5d5d74edbd48d782b3e94
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-09-10 13:36:23 -0400

    Handle Axios Error using Modal.

--- Burger/burger-builder/src/components/HOC/withErrorHandler/withErrorHandler.js
+++ Burger/burger-builder/src/components/HOC/withErrorHandler/withErrorHandler.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Modal from '../../UI/Modal/Modal';
 
@@ -9,15 +9,35 @@ import Modal from '../../UI/Modal/Modal';
 const withErrorHandler = (WrappedComponent, axios) => { // normally props are passed here.
 
     // return a FUNCTION which will be the "functional component" that takes props.
-    return (props) => { // functional component.
-        return ( 
-            <React.Fragment>
-                <Modal show>
-                    Something didn't work. Apologize.
-                </Modal>
-                <WrappedComponent {...props} /> 
-            </React.Fragment>
-        );
+    return class extends Component {
+        state = {
+            error: null
+        }
+        componentDidMount () {
+            axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req;
+            })
+            axios.interceptors.response.use(res=>res, error => {
+                this.setState({error: error});
+            });
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null});
+        }
+        render() {
+            return ( 
+                <React.Fragment>
+                    <Modal 
+                        show={this.state.error}
+                        modalClosed={this.errorConfirmedHandler}>
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props} /> 
+                </React.Fragment>
+            );
+        }
     }
 }
 
--- Burger/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -150,4 +150,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default withErrorHandler(BurgerBuilder);
+export default withErrorHandler(BurgerBuilder, axios);
