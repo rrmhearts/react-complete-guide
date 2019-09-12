02b2e8ce54217c07cefb8df063c3e956bb1385be
Ryan McCoppin <rrmhearts@gmail.com>
2019-09-12 13:05:15 -0400

    React Suspense Start.

--- HTTP/src/index.js
+++ HTTP/src/index.js
@@ -1,36 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Axios from 'axios';
 
-// Base URL for all paths.
-Axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-Axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-Axios.defaults.headers.post['Content-Type'] = 'application/json';
-// Interceptor
-Axios.interceptors.request.use(request => {
-    console.log('INTERCEPT:', request);
-    // Intercept and edit request config.
+ReactDOM.render(<App />, document.getElementById('root'));
 
-    return request;
-}, error => {
-    // Request SENDING fails
-    console.log('INTERCEPT:', error);
-    return Promise.reject(error);
-});
-
-Axios.interceptors.response.use(response => {
-    console.log('INTERCEPT:', response);
-    // Intercept and edit request config.
-
-    return response;
-}, error => {
-    // Request SENDING fails
-    console.log('INTERCEPT:', error);
-    return Promise.reject(error);
-});
-
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
-registerServiceWorker();
