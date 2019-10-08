100cb5726f4aa0ec28da2a8080a2c0dbb0731660
Ryan McCoppin <rrmhearts@gmail.com>
2019-10-08 14:51:03 -0400

    Add Redux to a React application. Basic.

--- Redux/src/containers/Counter/Counter.js
+++ Redux/src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -28,8 +29,8 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
                 <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
@@ -38,4 +39,20 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+// Passes Redux state to Counter as props.
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    };
+};
+
+// Passes info to reducer behind the scenes.
+const mapDispatchToProps = dispatch => {
+    return {
+        // adds to props.
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+    };
+};
+
+// HOC function that returns a HOC function
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- Redux/src/index.js
+++ Redux/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Store to hold state.
+const store = createStore(reducer);
+
+// Provider helps inject store into components.
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
