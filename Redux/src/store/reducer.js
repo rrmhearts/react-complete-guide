d9d7de13b0bdf50a62b12c056796554427cd4936
Ryan McCoppin <rrmhearts@gmail.com>
2019-10-08 15:28:56 -0400

    Old Reducer as switch.. Will be deleted.

--- Redux/src/store/reducer.js
+++ Redux/src/store/reducer.js
@@ -1,13 +1,26 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: [] // for button saving values...
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
+
+    // Switch reducer.
+    switch (action.type ) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.value
+            }
     }
+
     return state;
 };
 
