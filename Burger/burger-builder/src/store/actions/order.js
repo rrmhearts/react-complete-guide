0bd7ced94011962625ff135223b040d640888c6a
Ryan McCoppin <rrmhearts@gmail.com>
2019-10-23 11:05:02 -0400

    Show Orders by userId.

--- Burger/burger-builder/src/store/actions/order.js
+++ Burger/burger-builder/src/store/actions/order.js
@@ -72,11 +72,28 @@ export const fetchOrdersStart = () => {
     }
 }
 
-export const fetchOrders = (token) => {
+/* For getting userId by filter. Must change Firebase
+{
+  "rules": {
+    "ingredients" : {
+      ".read": true,
+      ".write": true,
+    },
+    "orders" : {
+      ".read": "auth != null",
+      ".write": "auth != null",
+      ".indexOn": ["userId"]
+    }
+  }
+}
+*/
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
         // Async order.
-        axios.get('/orders.json?auth=' + token)
+        const queryParams = '?auth=' + token + 
+            '&orderBy="userId"&equalTo="' + userId + '"';
+        axios.get('/orders.json' + queryParams)
             .then(res => {
                 const fetchedOrders = [];
                 for (let key in res.data) {
