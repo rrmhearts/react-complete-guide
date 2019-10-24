c8e5bb1b3ff3e3e21bfa33bd90f0e627f7618ce2
Ryan McCoppin <rrmhearts@gmail.com>
2019-10-24 15:20:21 -0400

    Webpack support for CSS.

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -2,6 +2,7 @@
 // Webpack runs on NODEJS!
 // Babel transpiles JS 6/7 into more traditional code
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     mode: 'development',
@@ -19,6 +20,27 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader', 
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[name]__[local]__[hash:base64:5]'
+                            }
+                        } 
+                    },
+                    { loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [autoprefixer()]
+                        }
+                    }
+                ]
             }
         ]
     }
