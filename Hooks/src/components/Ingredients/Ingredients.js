27a898ee4f705a1f99a746be91d54cba69613412
Ryan McCoppin <rrmhearts@gmail.com>
2019-11-11 11:01:03 -0500

    Search and useCallback

--- Hooks/src/components/Ingredients/Ingredients.js
+++ Hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,53 +7,33 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  /*
-    Manage SIDE EFFECTS like HTTP requests. Using to fetch data.
-    Occurs every render cycle. AFTER every render cycle.
-
-    Fetch without use effect will delay re-render.
-  */
-  useEffect(() => {
-
-    // Get ingredients
-    fetch('https://react-hooks-248c2.firebaseio.com/ingredients.json')
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        // for each item in database.
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key, /* unique id from firebase */
-            title: responseData[key].title, /* access json at key */
-            amount: responseData[key].amount
-          });
-        }
-        // AFTER request, update state. Have to render, infinite loop.
-        // Must use inside useEffect WITH 2nd argument.
-        setUserIngredients(loadedIngredients);
-      });
-  }, []); // only if 2nd arg CHANGES DO YOU RERENDER. Empty array, runs once acts like componentDidMount.
-
+  /* NOT NEEDED because ingredients are grabbed in search component from the start. */
+  
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', userIngredients);
-  }, [userIngredients]); // specify dependencies. will onlly run when userIngredients CHANGES.
+  }, [userIngredients]);
 
-  /* Async call, but delays rerender to fetch data.*/
+  /*
+    useCallback prevents function from being redeclared, never re-runs. It will survive re-render cycles.
+    Will not recreate function.
+  */
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients); // update list of ingredients component.
+  }, []);
+
   const addIngredientHandler = ingredient => {
-
-    // Browser Function to make HTTP request.
     fetch('https://react-hooks-248c2.firebaseio.com/ingredients.json', {
-      method: 'POST', // by default makes GET request. set to POST to post ingredients.
+      method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     })
       .then(response => {
-        return response.json(); // returns promise
+        return response.json();
       })
-      .then(responseData => { // process promise ^^ returned
+      .then(responseData => {
         setUserIngredients(prevIngredients => [
           ...prevIngredients,
-          { id: responseData.name /*firebase property uniq*/, ...ingredient }
+          { id: responseData.name, ...ingredient }
         ]);
       });
   };
@@ -69,7 +49,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList
           ingredients={userIngredients}
           onRemoveItem={removeIngredientHandler}
--- Hooks/src/components/Ingredients/Search.js
+++ Hooks/src/components/Ingredients/Search.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props; /* object destructuring */
+
+  // Set up with State.
+  const [enteredFilter, setEnteredFilter] = useState('');
+
+  /*
+    Will automatically occur when state changes. 
+    When search box changes, do this.
+    */
+  useEffect(() => {
+    // firebase support query parameters
+    const query =
+      enteredFilter.length === 0
+        ? ''
+        : `?orderBy="title"&equalTo="${enteredFilter}"`; // sort by title, filter by enteredFilter
+    // make call, requires firebase rules change
+    fetch('https://react-hooks-248c2.firebaseio.com/ingredients.json' + query)
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) { // all of this is the same as the other fetch calls.
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        onLoadIngredients(loadedIngredients);
+      });
+  }, [enteredFilter, onLoadIngredients]); /*only if onLoadIngredients changes. */
+  /* onLoadIngredients is recreated every time Ingredients function runs. */
+  /* this is prevented by using useCallback hook around function in Ingredients! Prevents infinite loop! */
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)} /* on filter value change, update state */
+          />
         </div>
       </Card>
     </section>
